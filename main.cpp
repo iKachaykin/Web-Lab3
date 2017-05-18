eaee727d6f4b6e0c19e971f36dcadab564f4528f
iKachaykin <ivankachaykin707@mail.ru>
2017-05-18 20:08:40 +0300

    New opportunities.
    Added an ability of multiple input.

--- main.cpp
+++ main.cpp
@@ -3,117 +3,186 @@
 #include <ctime>
 #include <algorithm>
 #include <cstdlib>
+#include <vector>
+
 using namespace std;
 
 bool is_all_ch_allowable(char *, const char *);
 
 void remove_ch(char *, char);
 
+void swap_ch(char *, char, char);
+
 void main() {
 	setlocale(LC_ALL, "ru");
 	srand(time(0));
 	const char DIGITS[] = "0123456789,."; 
 	const double EPS = 0.00000001;
-	char * params_string = getenv("QUERY_STRING");
+	char * params_string = getenv("QUERY_STRING"), *tmp_str = strtok(params_string, ";");
 	bool invalid_input = false;
-	unsigned short int number_of_p = 0, op_index = 0, p_index = 0, num_of_comas = 0, num_of_dots = 0;
+	unsigned short int number_of_p = 0, op_index = 0, p_index = 0;
 	double oper_param = 0, first_side = 0, second_side = 0, diagonal = 0;
+	vector<char *> *params_vector = new vector<char *>();
+	vector<parallelogram *> *parall_vector = new vector<parallelogram *>();
 	printf("Content-Type: text/html\r\n\r\n");
 	printf("<html> <head>\n");
 	printf("<title>Parallelograms</title>\n");
 	printf("</head>\n");
 	printf("<body>\n");
-	for (int i = 0; i < strlen(params_string); i++)
-		if (params_string[i] == ',')
-			num_of_comas++;
-	for (int i = 0; i < strlen(params_string); i++)
-		if (params_string[i] == '.')
-			num_of_dots++;
-	remove_ch(params_string, ' ');
-	invalid_input = !(is_all_ch_allowable(params_string, DIGITS)) || !strcmp(params_string, "") || !params_string || num_of_comas != 2 && num_of_comas != 3 || num_of_dots != 1 && num_of_dots != 0;
-	if (!invalid_input) {
-		char * tmp_str = strtok(params_string, ",");
-		number_of_p = strtol(tmp_str, NULL, 10);
-		parallelogram ** p_arr = new parallelogram*[number_of_p];
-		for (int i = 0; i < number_of_p; i++) {
-			first_side = rand() % 101 + rand() % 1001 / 1000.0;
-			second_side = rand() % 101 + rand() % 1001 / 1000.0;
-			if (ceil(max(first_side, second_side)) < floor(first_side + second_side)) {
-				diagonal = rand() % (int)(floor(first_side + second_side) - ceil(max(first_side, second_side)) + 1) + ceil(max(first_side, second_side));
-				diagonal += fabs(diagonal - floor(first_side + second_side)) < EPS ? -rand() % 1001 / 1000.0 : rand() % 1001 / 1000.0;
-			}
-			else 
-				diagonal = (max(first_side, second_side) + first_side + second_side) / 2;
-			p_arr[i] = new parallelogram(first_side, second_side, diagonal);
-			printf("<p>Параллелограм №%d - (первая сторона, вторая сторона, диагональ):%s</p>", i, p_arr[i]->to_string().c_str());
-		}
-		tmp_str = strtok(NULL, ",");
-		p_index = strtol(tmp_str, NULL, 10);
-		invalid_input = p_index >= number_of_p;
+	while (tmp_str) {
+		params_vector->push_back(tmp_str);
+		tmp_str = strtok(NULL, ";");
+	}
+	for (int str_i = 0; str_i < params_vector->size(); str_i++) {
+		remove_ch(params_vector->at(str_i), ' ');
+		invalid_input = !(is_all_ch_allowable(params_vector->at(str_i), DIGITS)) || !strcmp(params_vector->at(str_i), "") || !params_vector->at(str_i);
 		if (!invalid_input) {
-			tmp_str = strtok(NULL, ",");
+			tmp_str = strtok(params_vector->at(str_i), ",");
 			op_index = strtol(tmp_str, NULL, 10);
 			tmp_str = strtok(NULL, ",");
-			switch (op_index)
-			{
+			switch (op_index) {
 			case 1:
-				if (tmp_str) {
-					for (int i = 0; i < strlen(tmp_str); i++) {
-						if (tmp_str[i] == '.') {
-							tmp_str[i] = ',';
-							break;
-						}
-					}
-					oper_param = strtod(tmp_str, NULL);
-					p_arr[p_index]->size_variation(oper_param, 0);
-					printf("<p>Увеличили параллелограм №%d на %f</p><p>Измененный список параллелограммов:</p>", p_index, oper_param);
-					for (int i = 0; i < number_of_p; i++)
-						printf("<p>Параллелограм №%d - (первая сторона, вторая сторона, диагональ):%s</p>", i, p_arr[i]->to_string().c_str());
-				}
-				else
+				if (!tmp_str) {
 					invalid_input = true;
+					break;
+				}
+				swap_ch(tmp_str, ',', '.');
+				first_side = strtod(tmp_str, NULL);
+				tmp_str = strtok(NULL, ",");
+				if (!tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				swap_ch(tmp_str, ',', '.');
+				second_side = strtod(tmp_str, NULL);
+				tmp_str = strtok(NULL, ",");
+				if (!tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				swap_ch(tmp_str, ',', '.');
+				diagonal = strtod(tmp_str, NULL);
+				tmp_str = strtok(NULL, ",");
+				if (first_side + second_side > diagonal && first_side + diagonal > second_side && second_side + diagonal > first_side && !tmp_str)
+					parall_vector->push_back(new parallelogram(first_side, second_side, diagonal));
+				else {
+					invalid_input = true;
+					break;
+				}
 				break;
-
 			case 2:
-				if (tmp_str) {
-					for (int i = 0; i < strlen(tmp_str); i++) {
-						if (tmp_str[i] == '.') {
-							tmp_str[i] = ',';
-							break;
-						}
+				number_of_p = strtol(tmp_str, NULL, 10);
+				for (int i = 0; i < number_of_p; i++) {
+					first_side = rand() % 101 + rand() % 1001 / 1000.0;
+					second_side = rand() % 101 + rand() % 1001 / 1000.0;
+					if (ceil(max(first_side, second_side)) < floor(first_side + second_side)) {
+						diagonal = rand() % (int)(floor(first_side + second_side) - ceil(max(first_side, second_side)) + 1) + ceil(max(first_side, second_side));
+						diagonal += fabs(diagonal - floor(first_side + second_side)) < EPS ? -rand() % 1001 / 1000.0 : rand() % 1001 / 1000.0;
 					}
-					oper_param = strtod(tmp_str, NULL);
-					p_arr[p_index]->size_variation(oper_param, 1);
-					printf("<p>Уменьшили параллелограм №%d на %f</p><p>Измененный список параллелограммов:</p>", p_index, oper_param);
-					for (int i = 0; i < number_of_p; i++)
-						printf("<p>Параллелограм №%d - (первая сторона, вторая сторона, диагональ):%s</p>", i, p_arr[i]->to_string().c_str());
+					else
+						diagonal = (max(first_side, second_side) + first_side + second_side) / 2;
+					parall_vector->push_back(new parallelogram(first_side, second_side, diagonal));
 				}
-				else
-					invalid_input = true;
 				break;
 			case 3:
-				printf("<p>Квадратный корень периметра %d-го параллелограмма: %f</p>", p_index, p_arr[p_index]->perimeter_sqrt());
+				if (!tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				p_index = strtol(tmp_str, NULL, 10);
+				tmp_str = strtok(NULL, ",");
+				if (!tmp_str || p_index >= parall_vector->size() || p_index < 0) {
+					invalid_input = true;
+					break;
+				}
+				swap_ch(tmp_str, ',', '.');
+				oper_param = strtod(tmp_str, NULL);
+				tmp_str = strtok(NULL, ",");
+				if (tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				(*parall_vector)[p_index]->size_variation(oper_param, 0);
 				break;
 			case 4:
-				printf("<p>Квадратный корень площади %d-го параллелограмма: %f</p>", p_index, p_arr[p_index]->area_sqrt());
+				if (!tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				p_index = strtol(tmp_str, NULL, 10);
+				tmp_str = strtok(NULL, ",");
+				if (!tmp_str || p_index >= parall_vector->size() || p_index < 0) {
+					invalid_input = true;
+					break;
+				}
+				swap_ch(tmp_str, ',', '.');
+				oper_param = strtod(tmp_str, NULL);
+				tmp_str = strtok(NULL, ",");
+				if (tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				(*parall_vector)[p_index]->size_variation(oper_param, 1);
 				break;
 			case 5:
-				printf("<p>Вторая диагональ %d-го параллелограмма: %f</p>", p_index, p_arr[p_index]->get_diagonal());
+				if (tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				if (parall_vector->size()) {
+					printf("<table cellpadding=\"10\" cellspacing=\"0\" border = \"1\">");
+					printf("<tr>%s</tr><tr>%s%s%s%s%s%s%s%s%s</tr>",
+						"<td colspan = \"9\">Список параллелограммов</td>",
+						"<th>Номер параллелограмма</th>",
+						"<th>Первая сторона</th>",
+						"<th>Вторая сторона</th>",
+						"<th>Первая диагональ</th>",
+						"<th>Вторая диагональ</th>",
+						"<th>Высота на первую сторону</th>",
+						"<th>Высота на вторую сторону</th>",
+						"<th>Квадратный корень периметра</th>",
+						"<th>Квадратный корень площади</th>");
+					for (int i = 0; i < parall_vector->size(); i++) {
+						printf("<tr>");
+						printf("<th>%d</th><th>%f</th><th>%f</th><th>%f</th><th>%f</th><th>%f</th><th>%f</th><th>%f</th><th>%f</th>", i,
+							parall_vector->at(i)->get_first_side(), parall_vector->at(i)->get_second_side(), parall_vector->at(i)->get_first_diagonal(),
+							parall_vector->at(i)->get_second_diagonal(), parall_vector->at(i)->get_height_on_first_side(), parall_vector->at(i)->get_height_on_second_side(),
+							parall_vector->at(i)->perimeter_sqrt(), parall_vector->at(i)->area_sqrt());
+						printf("</tr>");
+					}
+					printf("<p>\r\n</p>");
+				}
+				else
+					printf("<p>Список параллелограммов пуст.</p>");
 				break;
 			case 6:
-				printf("<p>Высота на первую сторону %d-го параллелограмма: %f</p>", p_index, p_arr[p_index]->get_height_on_first_side());
+				if (!tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				p_index = strtol(tmp_str, NULL, 10);
+				tmp_str = strtok(NULL, ",");
+				if (tmp_str || p_index >= parall_vector->size() || p_index < 0) {
+					invalid_input = true;
+					break;
+				}
+				parall_vector->erase(parall_vector->begin() + p_index);
 				break;
 			case 7:
-				printf("<p>Высота на вторую сторону %d-го параллелограмма: %f</p>", p_index, p_arr[p_index]->get_height_on_second_side());
+				if (tmp_str) {
+					invalid_input = true;
+					break;
+				}
+				parall_vector->clear();
 				break;
 			default:
 				invalid_input = true;
 				break;
 			}
 		}
+		if (invalid_input)
+			printf("<h1>ОШИБКА!!</h1><p>Один, или несколько параметров были введены неверно в наборе параметров №%d.</p>", str_i + 1);
 	}
-	if (invalid_input)
-		printf("<h1>ОШИБКА!!</h1><p>Один, или несколько параметров были введены неверно.</p>");
 	printf("</body> </html>\n");
 }
 
@@ -124,7 +193,7 @@ bool is_all_ch_allowable(char * str, const char * allowable_ch) {
 	return true;
 }
 
-void remove_ch(char * str, char ch_del) {
+void remove_ch(char *str, char ch_del) {
 	char *buff = new char[strlen(str)];
 	_strset(buff, 0);
 	while (strchr(str, ch_del)) {
@@ -133,3 +202,10 @@ void remove_ch(char * str, char ch_del) {
 		strcat(str, buff);
 	}
 }
+
+void swap_ch(char * str, char new_char, char old_char) {
+	for (int i = 0; i < strlen(str); i++)
+		if (str[i] == old_char)
+			str[i] = new_char;
+}
+
